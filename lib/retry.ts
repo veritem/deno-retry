6cda428d1059bb72b531846593acb5885cdc7696
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-02-20 15:59:03 +0200

    feat: fighting with ci

---
+++ lib/retry.ts
@@ -0,0 +1,26 @@
+//@copyright 2021  deno-rety by Makuza Mugabo Verite
+
+import { RetryOperation } from "./retry_operation.ts";
+
+
+function timeOutsHandler(options:unknown){
+
+}
+
+
+export function operation(options){
+
+    // if(options instanceof Array){
+    //     return [].concat(options)
+    // }
+
+//     let timeOuts = timeOutsHandler(options)
+
+//   return new RetryOperation(
+// timeOuts,
+// {
+
+// }
+//   )
+
+}
